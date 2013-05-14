c351e79cfbfa820db582a3c65137efd25d11c51e
Johan Gustafsson <johan@tinyrocket.se>
2013-05-14 20:51:28 +0200

    some documentation for the shader preprocessor

--- Triton.Graphics/Shaders/Preprocessor.cs
+++ Triton.Graphics/Shaders/Preprocessor.cs
@@ -7,6 +7,23 @@ using System.Text.RegularExpressions;
 
 namespace Triton.Graphics.Shaders
 {
+	/// <summary>
+	/// Processes glsl shaders
+	/// 
+	/// Handles the following preprocessor commands
+	/// * import(absolute_path_without_extension) 
+	///		- Includes a glsl file into the currently processed file, included files are not expanded by the preprocessor
+	///		- Example: import(shaders/lighting/cook_torrance) => all code in cook_torrance.glsl
+	///	* attrib(type, name, externalType)
+	///		- Declares an attrib (in) variable, the attrib will be bound to a attrib location determined by externalType
+	///		- type is a glsl type, ie vec3, mat4x4
+	///		- externalType is any of Position, Normal, Tangent, TexCoord
+	///		- Example: attrib(vec3, iPosition, Position) => in vec3 iPosition;
+	///	 * uniform(type, name, externalName)
+	///		- Declares a uniform and exposes it to the code, might be automatically bound depending on the external name.
+	///		  Any automaitc bounding depends on the implementing application.
+	///		- Example: uniform(vec3, lightColor, LightColor) => uniform vec3 lightColor
+	/// </summary>
 	class Preprocessor
 	{
 		private readonly Triton.Common.IO.FileSystem FileSystem;
