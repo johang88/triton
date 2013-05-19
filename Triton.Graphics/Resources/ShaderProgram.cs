307034049f6eb933b36063b9b06f721fd3c21bc9
Johan Gustafsson <johan@tinyrocket.se>
2013-05-19 19:50:42 +0200

    Fixed #17

--- Triton.Graphics/Resources/ShaderProgram.cs
+++ Triton.Graphics/Resources/ShaderProgram.cs
@@ -9,8 +9,8 @@ namespace Triton.Graphics.Resources
 	public class ShaderProgram : Triton.Common.Resource
 	{
 		public int Handle { get; internal set; }
-		private Dictionary<Triton.Common.HashedString, int> Uniforms = new Dictionary<Common.HashedString, int>();
-		private Dictionary<string, string> BindNamesToVarNames = new Dictionary<string, string>();
+		private Dictionary<Common.HashedString, int> Uniforms = new Dictionary<Common.HashedString, int>();
+		private Dictionary<Common.HashedString, Common.HashedString> BindNamesToVarNames = new Dictionary<Common.HashedString, Common.HashedString>();
 		private readonly Backend Backend;
 
 		public ShaderProgram(string name, string parameters, Backend backend)
@@ -23,10 +23,10 @@ namespace Triton.Graphics.Resources
 		internal void Reset()
 		{
 			Uniforms = new Dictionary<Common.HashedString, int>();
-			BindNamesToVarNames = new Dictionary<string, string>();
+			BindNamesToVarNames = new Dictionary<Common.HashedString, Common.HashedString>();
 		}
 
-		public int GetUniform(Triton.Common.HashedString name)
+		public int GetUniform(Common.HashedString name)
 		{
 			int uniformLocation;
 			if (!Uniforms.TryGetValue(name, out uniformLocation))
@@ -38,23 +38,20 @@ namespace Triton.Graphics.Resources
 			return uniformLocation;
 		}
 
+		public int GetAliasedUniform(Common.HashedString name)
+		{
+			return GetUniform(BindNamesToVarNames[name]);
+		}
+
+		public bool HasAliasedUniform(Common.HashedString name)
+		{
+			return BindNamesToVarNames.ContainsKey(name);
+		}
+
 		public void AddUniform(string bindName, string name)
 		{
 			BindNamesToVarNames.Add(bindName, name);
 		}
 
-		/// <summary>
-		/// Get a list of key-value-pairs of external uniform names to internal uniform names
-		/// Ie:
-		/// uniform(vec3, lightDir, LightDirection) will result in ("LightDirection", "lightDir")
-		/// 
-		/// This can be used to automatically resolve uniform locations and to keep the actual variable
-		/// names in the shader independant of the data that they should be bound to.
-		/// </summary>
-		/// <returns></returns>
-		public IEnumerable<KeyValuePair<string, string>> GetUniforms()
-		{
-			return BindNamesToVarNames;
-		}
 	}
 }
