b9a6b76025e26ce4f96c2c6ac510e4583d0f8eaf
Johan Gustafsson <johan@tinyrocket.se>
2013-05-19 18:28:17 +0200

    Identity quaternion

--- Triton.Math/Quaternion.cs
+++ Triton.Math/Quaternion.cs
@@ -15,6 +15,8 @@ namespace Triton
 		public readonly float Z;
 		public readonly float W;
 
+		public static readonly Quaternion Identity = new Quaternion(0, 0, 0, 1);
+
 		public Quaternion(float x, float y, float z, float w)
 		{
 			X = x;
