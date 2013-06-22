17bd8aff890e32eed38c355834b9507eea32f05c
Johan Gustafsson <johan@tinyrocket.se>
2013-06-23 00:28:35 +0200

    improved the ogre xml parser

--- Triton.Content/Meshes/Converters/OgreXmlConverter.cs
+++ Triton.Content/Meshes/Converters/OgreXmlConverter.cs
@@ -47,27 +47,6 @@ namespace Triton.Content.Meshes.Converters
 
 					subMesh.TriangleCount = faceCount;
 
-					reader.ReadToFollowing("geometry");
-					int vertexCount = int.Parse(reader.GetAttribute("vertexcount"), CultureInfo.InvariantCulture);
-
-					reader.ReadToFollowing("vertexbuffer");
-					bool positions = bool.Parse(reader.GetAttribute("positions"));
-					bool normals = bool.Parse(reader.GetAttribute("normals"));
-					bool tangents = bool.Parse(reader.GetAttribute("tangents"));
-					int textureCoords = int.Parse(reader.GetAttribute("texture_coords"), CultureInfo.InvariantCulture);
-
-					if (!positions)
-						throw new ArgumentException("invalid mesh, no positions");
-
-					if (!normals)
-						throw new ArgumentException("invalid mesh, no normals");
-
-					if (textureCoords == 0)
-						throw new ArgumentException("invalid mesh, no texcoords");
-
-					if (!tangents)
-						throw new ArgumentException("invalid mesh, no tangents");
-
 					subMesh.VertexFormat = new Renderer.VertexFormat(new Renderer.VertexFormatElement[]
 						{
 							new Renderer.VertexFormatElement(Renderer.VertexFormatSemantic.Position, Renderer.VertexPointerType.Float, 3, 0),
@@ -76,27 +55,49 @@ namespace Triton.Content.Meshes.Converters
 							new Renderer.VertexFormatElement(Renderer.VertexFormatSemantic.TexCoord, Renderer.VertexPointerType.Float, 2, sizeof(float) * 9),
 						});
 
+					reader.ReadToFollowing("geometry");
+					int vertexCount = int.Parse(reader.GetAttribute("vertexcount"), CultureInfo.InvariantCulture);
+
+					var vertices = new Vertex[vertexCount];
+
+					while (reader.ReadToFollowing("vertexbuffer"))
+					{
+						for (int i = 0; i < vertexCount; i++)
+						{
+							var vertex = vertices[i];
+
+							reader.ReadToFollowing("vertex");
+							var subReader = reader.ReadSubtree();
+
+							while (subReader.Read())
+							{
+								if (subReader.NodeType != XmlNodeType.Element)
+									continue;
+
+								if (subReader.Name == "position")
+									vertex.Position = ReadVector3(reader);
+								else if (subReader.Name == "normal")
+									vertex.Normal = ReadVector3(reader);
+								else if (subReader.Name == "tangent")
+									vertex.Tangent = ReadVector3(reader);
+								else if (subReader.Name == "texcoord")
+									vertex.TexCoord = ReadUV(reader);
+							}
+
+							vertices[i] = vertex;
+						}
+					}
+
 					using (var memStream = new MemoryStream(vertexCount * (3 * sizeof(float) + 3 * sizeof(float) + 3 * sizeof(float) + 2 * sizeof(float))))
 					{
 						using (var writer = new BinaryWriter(memStream))
 						{
 							for (int i = 0; i < vertexCount; i++)
 							{
-								reader.ReadToFollowing("vertex");
-
-								reader.ReadToFollowing("position");
-								writer.Write(ReadVector3(reader));
-
-								reader.ReadToFollowing("normal");
-								writer.Write(ReadVector3(reader));
-
-								reader.ReadToFollowing("texcoord");
-								var texCoord = ReadUV(reader);
-
-								reader.ReadToFollowing("tangent");
-								writer.Write(ReadVector3(reader));
-
-								writer.Write(texCoord);
+								writer.Write(vertices[i].Position);
+								writer.Write(vertices[i].Normal);
+								writer.Write(vertices[i].Tangent);
+								writer.Write(vertices[i].TexCoord);
 							}
 
 							subMesh.Vertices = memStream.GetBuffer();
@@ -123,6 +124,14 @@ namespace Triton.Content.Meshes.Converters
 		Triton.Vector2 ReadUV(XmlReader reader)
 		{
 			return new Triton.Vector2(ParseFloat(reader.GetAttribute("u")), ParseFloat(reader.GetAttribute("v")));
+		}
+
+		struct Vertex
+		{
+			public Vector3 Position;
+			public Vector3 Normal;
+			public Vector3 Tangent;
+			public Vector2 TexCoord;
 		}
 	}
 }
