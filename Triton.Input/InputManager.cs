8c6f7d3b66c996d8687858859839f72208e1a6d7
Johan Gustafsson <johan@tinyrocket.se>
2013-12-22 00:12:27 +0100

    InputManager no longer uses System.Windows.Forms - Fixed #53

--- Triton.Input/InputManager.cs
+++ Triton.Input/InputManager.cs
@@ -3,40 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 using OpenTK.Input;
 using System.Drawing;
-using System.Windows.Forms;
 
 namespace Triton.Input
 {
     public class InputManager
     {
 		public Rectangle Bounds;
-		private Point OldMousePosition;
+		private Vector2 OldMousePosition;
 
 		private Vector2 _MouseDelta;
 		public Vector2 MouseDelta { get { return _MouseDelta; } }
-
-		public bool LockMouse = true;
 
 		public InputManager(Rectangle bounds)
 		{
 			Bounds = bounds;
 
-			Cursor.Position = new Point(Bounds.Left + (Bounds.Width / 2), Bounds.Top + (Bounds.Height / 2));
-			OldMousePosition = Cursor.Position;
+			Mouse.SetPosition(0, 0);
+			OldMousePosition = new Vector2(0, 0);
 		}
 
 		public void Update()
 		{
-			_MouseDelta = new Vector2(Cursor.Position.X - OldMousePosition.X, Cursor.Position.Y - OldMousePosition.Y);
+			var state = Mouse.GetState();
 
-			if (LockMouse)
-			{
-				Cursor.Position = OldMousePosition = new Point(Bounds.Left + (Bounds.Width / 2), Bounds.Top + (Bounds.Height / 2));
-			}
-			else
-			{
-				OldMousePosition = Cursor.Position;
-			}
+			_MouseDelta = new Vector2(state.X - OldMousePosition.X, state.Y - OldMousePosition.Y);
+
+			OldMousePosition.X = state.X;
+			OldMousePosition.Y = state.Y;
 		}
 
 		public bool IsKeyDown(Key key)
