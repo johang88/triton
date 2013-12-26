b7ba3cc4268821408831771ab836a49a4fce43de
Johan Gustafsson <johan@tinyrocket.se>
2013-12-26 11:28:13 +0100

    more stuff in Math.Utili

--- Triton.Math/Util.cs
+++ Triton.Math/Util.cs
@@ -8,6 +8,29 @@ namespace Triton.Math
 {
 	public class Util
 	{
+		public const float Pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930382f;
+		public const float PiOver2 = Pi / 2;
+		public const float PiOver3 = Pi / 3;
+		public const float PiOver4 = Pi / 4;
+		public const float PiOver6 = Pi / 6;
+		public const float TwoPi = 2 * Pi;
+		public const float ThreePiOver2 = 3 * Pi / 2;
+		public const float E = 2.71828182845904523536f;
+		public const float Log10E = 0.434294482f;
+		public const float Log2E = 1.442695041f;
+
+		public static float DegreesToRadians(float degrees)
+		{
+			const float degToRad = (float)System.Math.PI / 180.0f;
+			return degrees * degToRad;
+		}
+
+		public static float RadiansToDegrees(float radians)
+		{
+			const float radToDeg = 180.0f / (float)System.Math.PI;
+			return radians * radToDeg;
+		}
+
 		public static float Lerp(float x, float y, float a)
 		{
 			return x + a * (y - x);
