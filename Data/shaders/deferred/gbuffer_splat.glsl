13526a996e184765be8afa5399a155398bf1f878
Johan Gustafsson <johan@tinyrocket.se>
2013-12-27 17:22:05 +0100

    terrain splat shader

---
+++ Data/shaders/deferred/gbuffer_splat.glsl
@@ -0,0 +1,101 @@
+#ifdef VERTEX_SHADER
+
+attrib(vec3, iPosition, Position);
+attrib(vec2, iTexCoord, TexCoord);
+attrib(vec3, iNormal, Normal);
+attrib(vec3, iTangent, Tangent);
+
+out vec3 normal;
+out vec3 tangent;
+out vec3 bitangent;
+out vec2 texCoord;
+out vec2 texCoordDetail;
+out vec4 position;
+
+uniform(mat4x4, world, World);
+uniform(mat4x4, worldView, WorldView);
+uniform(mat4x4, modelViewProjection, ModelViewProjection);
+
+#ifdef SKINNED
+uniform(mat4x4[64], bones, Bones);
+#endif
+
+void main()
+{
+	texCoord = iTexCoord;
+	texCoordDetail = iTexCoord * 32;
+	
+	normal = normalize(iNormal);
+	tangent = normalize(iTangent);
+	bitangent = normalize(cross(normal, tangent));
+	
+	position = worldView * vec4(iPosition, 1);
+	
+	gl_Position = modelViewProjection * vec4(iPosition, 1);
+}
+
+#else
+
+import(shaders/utility/utils);
+
+in vec3 normal;
+in vec3 tangent;
+in vec3 bitangent;
+in vec2 texCoord;
+in vec2 texCoordDetail;
+in vec4 position;
+
+out(vec4, oColor, 0);
+out(vec4, oNormal, 1);
+out(vec4, oPosition, 2);
+out(vec4, oSpecular, 3);
+
+sampler(2D, samplerSplat, SplatTexture);
+sampler(2D, samplerNormal1, NormalMap1);
+sampler(2D, samplerNormal2, NormalMap2);
+sampler(2D, samplerNormal3, NormalMap3);
+sampler(2D, samplerNormal4, NormalMap4);
+sampler(2D, samplerDiffuse1, Diffuse1);
+sampler(2D, samplerDiffuse2, Diffuse2);
+sampler(2D, samplerDiffuse3, Diffuse3);
+sampler(2D, samplerDiffuse4, Diffuse4);
+
+uniform(mat4x4, itWorldView, ITWorldView);
+
+void main()
+{
+	vec4 splat = texture2D(samplerSplat, texCoord);
+	
+	vec4 n1 = texture2D(samplerNormal1, texCoordDetail);
+	vec4 n2 = texture2D(samplerNormal2, texCoordDetail);
+	vec4 n3 = texture2D(samplerNormal3, texCoordDetail);
+	vec4 n4 = texture2D(samplerNormal4, texCoordDetail);
+	
+	vec4 normalSpecular = n1 * splat.x + n2 * splat.y + n3 * splat.z + n4 * splat.w;
+	vec3 N = normalize(normalSpecular.xyz * 2.0f - 1.0f);
+
+	mat3x3 rot = mat3x3(normalize(tangent), normalize(bitangent), normalize(normal));
+
+	vec3 N2 = normalize(rot * N);
+	N2 = normalize(mat3x3(itWorldView) * N2);
+
+	vec4 d1 = texture2D(samplerDiffuse1, texCoordDetail);
+	vec4 d2 = texture2D(samplerDiffuse2, texCoordDetail);
+	vec4 d3 = texture2D(samplerDiffuse3, texCoordDetail);
+	vec4 d4 = texture2D(samplerDiffuse4, texCoordDetail);
+	
+	vec4 diffuse = d1 * splat.x + d2 * splat.y + d3 * splat.z + d4 * splat.w;
+	
+	vec4 specular = diffuse;
+	specular.w = normalSpecular.w;
+	
+	vec3 gamma = (2.2f).xxx;
+	diffuse.xyz = pow(diffuse.xyz, gamma);
+	specular.xyz = pow(specular.xyz, gamma);
+	
+	oColor = vec4(diffuse.xyz, 1.0f);
+	oNormal = vec4(N2.xyz, 1.0f);
+	oSpecular = specular;
+	oPosition = position;
+}
+#endif
--- Triton.Graphics/Resources/MaterialLoader.cs
+++ Triton.Graphics/Resources/MaterialLoader.cs
@@ -13,6 +13,7 @@ namespace Triton.Graphics.Resources
 		private readonly Triton.Common.ResourceManager ResourceManager;
 
 		private const string DefaultShader = "shaders/deferred/gbuffer";
+		private const string DefaultSplatShader = "shaders/deferred/gbuffer_splat";
 
 		public MaterialLoader(Triton.Common.ResourceManager resourceManager, Triton.Common.IO.FileSystem fileSystem)
 		{
@@ -35,38 +36,85 @@ namespace Triton.Graphics.Resources
 				var definition = JsonObject.Parse(reader.ReadToEnd());
 				var type = definition.Get("type") ?? "standard";
 
-				var isSkinned = Common.StringConverter.ParseBool(definition.Get("is_skinned") ?? "false");
+				if (type == "splat")
+				{
+					Material material = new Materials.SplatMaterial(name, parameters, ResourceManager);
+					material.Definition = definition;
 
-				Material material = new Materials.StandardMaterial(name, parameters, ResourceManager, isSkinned);
-				material.Definition = definition;
+					return material;
+				}
+				else
+				{
+					var isSkinned = Common.StringConverter.ParseBool(definition.Get("is_skinned") ?? "false");
 
-				return material;
+					Material material = new Materials.StandardMaterial(name, parameters, ResourceManager, isSkinned);
+					material.Definition = definition;
+
+					return material;
+				}
 			}
 		}
 
 		public void Load(Common.Resource resource, string parameters, Action<Common.Resource> onLoaded)
 		{
-			var material = (Materials.StandardMaterial)resource;
-			var filename = resource.Name + ".mat.v";
+			if (resource is Materials.SplatMaterial)
+			{
+				var material = (Materials.SplatMaterial)resource;
+				var filename = resource.Name + ".mat.v";
 
-			var definition = material.Definition;
+				var definition = material.Definition;
 
-			if (!string.IsNullOrWhiteSpace(definition.Get("diffuse")))
-				material.Diffuse = ResourceManager.Load<Texture>(definition.Get("diffuse"), "srgb");
-			if (!string.IsNullOrWhiteSpace(definition.Get("normal")))
-				material.Normal = ResourceManager.Load<Texture>(definition.Get("normal"));
-			if (!string.IsNullOrWhiteSpace(definition.Get("gloss")))
-				material.Gloss = ResourceManager.Load<Texture>(definition.Get("gloss"));
-			if (!string.IsNullOrWhiteSpace(definition.Get("specular")))
-				material.Specular = ResourceManager.Load<Texture>(definition.Get("specular"));
-				
-			var shader = DefaultShader;
-			if (!string.IsNullOrEmpty(definition.Get("shader")))
-				shader = definition.Get("shader");
+				if (!string.IsNullOrWhiteSpace(definition.Get("diffuse1")))
+					material.Diffuse1 = ResourceManager.Load<Texture>(definition.Get("diffuse1"), "srgb");
+				if (!string.IsNullOrWhiteSpace(definition.Get("diffuse2")))
+					material.Diffuse2 = ResourceManager.Load<Texture>(definition.Get("diffuse2"), "srgb");
+				if (!string.IsNullOrWhiteSpace(definition.Get("diffuse3")))
+					material.Diffuse3 = ResourceManager.Load<Texture>(definition.Get("diffuse3"), "srgb");
+				if (!string.IsNullOrWhiteSpace(definition.Get("diffuse4")))
+					material.Diffuse4 = ResourceManager.Load<Texture>(definition.Get("diffuse4"), "srgb");
+				if (!string.IsNullOrWhiteSpace(definition.Get("normal1")))
+					material.Normal1 = ResourceManager.Load<Texture>(definition.Get("normal1"));
+				if (!string.IsNullOrWhiteSpace(definition.Get("normal2")))
+					material.Normal2 = ResourceManager.Load<Texture>(definition.Get("normal2"));
+				if (!string.IsNullOrWhiteSpace(definition.Get("normal3")))
+					material.Normal3 = ResourceManager.Load<Texture>(definition.Get("normal3"));
+				if (!string.IsNullOrWhiteSpace(definition.Get("normal4")))
+					material.Normal4 = ResourceManager.Load<Texture>(definition.Get("normal4"));
+				if (!string.IsNullOrWhiteSpace(definition.Get("splat")))
+					material.Splat = ResourceManager.Load<Texture>(definition.Get("splat"));
 
-			material.Shader = ResourceManager.Load<ShaderProgram>(shader, material.IsSkinned ? "SKINNED" : "");
+				var shader = DefaultSplatShader;
+				if (!string.IsNullOrEmpty(definition.Get("shader")))
+					shader = definition.Get("shader");
 
-			onLoaded(material);
+				material.Shader = ResourceManager.Load<ShaderProgram>(shader);
+
+				onLoaded(material);
+			}
+			else
+			{
+				var material = (Materials.StandardMaterial)resource;
+				var filename = resource.Name + ".mat.v";
+
+				var definition = material.Definition;
+
+				if (!string.IsNullOrWhiteSpace(definition.Get("diffuse")))
+					material.Diffuse = ResourceManager.Load<Texture>(definition.Get("diffuse"), "srgb");
+				if (!string.IsNullOrWhiteSpace(definition.Get("normal")))
+					material.Normal = ResourceManager.Load<Texture>(definition.Get("normal"));
+				if (!string.IsNullOrWhiteSpace(definition.Get("gloss")))
+					material.Gloss = ResourceManager.Load<Texture>(definition.Get("gloss"));
+				if (!string.IsNullOrWhiteSpace(definition.Get("specular")))
+					material.Specular = ResourceManager.Load<Texture>(definition.Get("specular"));
+
+				var shader = DefaultShader;
+				if (!string.IsNullOrEmpty(definition.Get("shader")))
+					shader = definition.Get("shader");
+
+				material.Shader = ResourceManager.Load<ShaderProgram>(shader, material.IsSkinned ? "SKINNED" : "");
+
+				onLoaded(material);
+			}
 		}
 
 		public void Unload(Common.Resource resource)
