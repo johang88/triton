776039e7c906c881764b302a467c35682822486d
Johan Gustafsson <johan@tinyrocket.se>
2014-08-24 14:28:08 +0200

    Added per instance semantics for vertex format #91

--- Triton.Renderer/VertexFormatSemantic.cs
+++ Triton.Renderer/VertexFormatSemantic.cs
@@ -16,6 +16,13 @@ namespace Triton.Renderer
 		Color,
 		BoneIndex,
 		BoneWeight,
+		// InstanceTransform0-3 stores a per instance 4x4 matrix
+		InstanceTransform0,
+		InstanceTransform1,
+		InstanceTransform2,
+		InstanceTransform3,
+		// Can be used to store extra per instance data like texture id etc ... 
+		InstanceMisc,
 		Last
 	}
 }
