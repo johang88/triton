fde9d959a28af681cdae1278a256958030708a98
Johan Gustafsson <johan@tinyrocket.se>
2019-01-12 17:53:20 +0100

    Removed old material files

--- Data/samples_data/materials/sphere.mat.v
+++ Data/samples_data/materials/sphere.mat.v
@@ -2,8 +2,8 @@
     "Shader": "/shaders/deferred/gbuffer",
 	"Uniforms": {
 		"DiffuseColor": "0.972 0.960 0.915 1",
-		"Metalness": 1.0,
-		"Roughness": 0.1
+		"Metalness": 0.0,
+		"Roughness": 0.2
 	},
 	"Textures": {
 		"NormalMap": "/textures/random_n"
